db2f5500465e7521fb040d9a1bccc5b9206279ed
Abel Gonzalo Barba <abel.gonzalo@optivamedia.com>
2023-03-02 16:47:42 +0100

    restart from scrap

--- src/funciones.js
+++
@@ -1,8 +0,0 @@
-const volts = document.querySelector('#volts');
-const amps = document.querySelector('#amps');
-const pow = document.querySelector('#pow');
-const btnAgregar = document.querySelector('#btn_agregar');
-
-btnAgregar.addEventListener('click', function(){
-    window.location.href = `agregar/${volts.value}/${amps.value}/${pow.value}`;
-});
