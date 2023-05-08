1c24f843694352bc0814648801ce65326b9bad78
Abel Gonzalo Barba <abel.gonzalo@optivamedia.com>
2023-05-08 17:56:49 +0200

    Index template

--- Server/server.py
+++ Server/server.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template 
 from pysondb import db
 from datetime import datetime
 
@@ -8,19 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # TODO render html view
-    return "HELLO WORLD"
+    return render_template("index.html")
 
-@app.route('/getData/<startDate>/<endDate>')
+@app.route('/getData')
 def getData(startDate='a', endDate='b'):
     results = ina226db.getAll()
-
+    startDate = request.args.get('startDate')
+    endDate = request.args.get('endDate')
     filteredResults = []
     for result in results:
         if startDate <= result["date"] <= endDate:
             filteredResults.append(result)
 
-    return filteredResults
+    dates = [row["date"] for row in filteredResults]
+    volts = [row["volts"] for row in filteredResults]
+    amps = [row["amps"] for row in filteredResults]
+    pow = [row["pow"] for row in filteredResults]
+    
+    return render_template("index.html", dates=dates, volts=volts, amps=amps, pow=pow)
 
 @app.route('/insertData/<volts>/<amps>/<pow>')
 def insertData(volts=0, amps=0, pow=0):
@@ -30,6 +34,7 @@ def insertData(volts=0, amps=0, pow=0):
         "pow": pow,
         "date": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     })
+    
     return "NEW DATA INSERTED"
 
-app.run(port=8000, debug=True)
+app.run(host='0.0.0.0', port=8000, debug=True)
