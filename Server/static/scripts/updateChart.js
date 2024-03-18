a331521ec7fcf7d54f777b604e2f72a47048dda6
Abel Gonzalo Barba <abel.gonzalo@optivamedia.com>
2024-03-18 20:38:23 +0100

    Add the rest of charts and the temperatures in live section

--- Server/static/scripts/updateChart.js
+++ Server/static/scripts/updateChart.js
@@ -1,92 +1,70 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
-var ctx1 = document.getElementById('ina1').getContext('2d');
-var ctx2 = document.getElementById('ina2').getContext('2d');
-const maxDataPoints = 10;
+var charts = [];
+const maxDataPoints = 50;
 
-var ina1 = new Chart(ctx1, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Volts',
-            borderColor: "rgb(255,0,0)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            data: [],
-        }, 
-        {
-            label: 'Amps',
-            borderColor: "rgb(255,255,0)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            data: [],
-        },
-        {
-            label: "Pow",
-            borderColor: "rgb(0,0,255)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            lineTension: 0.5
-        }]
-    },
-    options: {
-        responsive: true,
-        animation: false
+function initCharts() {
+    for (var i = 1; i <= 5; i++) {
+        var ctx = document.getElementById('ina' + i).getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{
+                        label: 'Volts',
+                        borderColor: "rgb(255,0,0)",
+                        pointRadius: 5,
+                        pointHoverRadius: 10,
+                        data: [],
+                    },
+                    {
+                        label: 'Amps',
+                        borderColor: "rgb(255,255,0)",
+                        pointRadius: 5,
+                        pointHoverRadius: 10,
+                        data: [],
+                    },
+                    {
+                        label: "Pow",
+                        borderColor: "rgb(0,0,255)",
+                        pointRadius: 5,
+                        pointHoverRadius: 10,
+                        lineTension: 0.5
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                animation: false
+            }
+        });
+        charts.push(chart);
     }
-});
+}
 
-var ina2 = new Chart(ctx2, {
-    type: 'line',
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Volts',
-            borderColor: "rgb(255,0,0)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            data: [],
-        }, 
-        {
-            label: 'Amps',
-            borderColor: "rgb(255,255,0)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            data: [],
-        },
-        {
-            label: "Pow",
-            borderColor: "rgb(0,0,255)",
-            pointRadius: 5,
-            pointHoverRadius: 10,
-            lineTension: 0.5
-        }]
-    },
-    options: {
-        responsive: true,
-        animation: false
-    }
-});
+initCharts();
 
 socket.on('update_chart', function(data) {
     var chart = null;
+
     switch (data.sensor) {
         case 'INA1':
-            chart = ina1;
+            chart = charts[0];
             break;
         case 'INA2':
-            chart = ina2;
+            chart = charts[1];
             break;
         case 'INA3':
-            chart = ina3;
+            chart = charts[2];
             break;
         case 'INA4':
-            chart = ina4;
+            chart = charts[3];
             break;
         case 'INA5':
-            chart = ina5;
+            chart = charts[4];
             break;
     }
-    chart.data.labels.push(data.date);
+
+    chart.data.labels.push(data.date.split(" ")[1]);
     chart.data.datasets[0].data.push(data.volts);
     chart.data.datasets[1].data.push(data.amps);
     chart.data.datasets[2].data.push(data.pow);
